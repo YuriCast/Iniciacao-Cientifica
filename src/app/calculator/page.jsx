7f6e1d20f1ecb205e2ae8e4e64bfbb6a955e0576
Yuri Castilho <yuricastilho2005@gmail.com>
2023-09-04 03:07:54 -0300

    style: estilizar tela calculator

--- src/app/calculator/page.jsx
+++ src/app/calculator/page.jsx
@@ -1,52 +1,34 @@
-import React from 'react'
-import styles from './page.module.css'
+'use client'
+import React, { useState } from 'react';
+import styles from './page.module.css';
 
-const Calculator = () => {
+export default function Calculator() {
   return (
     <div className={styles.main}>
-      <div className={styles.item1}>
-        <div className={styles.teste}>
-        <h3 className={styles.titulo}>
-          Calculadora
-        </h3>
-        <p>
-          Modelo
-        </p>
-        <input type="text" className={styles.input}/>
-        <p className={styles.subtitulo}>
-          Qtd. Veículos
-        </p>
-        <input type="number" className={styles.input}/>
-        <p className={styles.subtitulo}>
-          Distância
-       </p>
-        <input type="number" className={styles.input}/>
-        <p className={styles.subtitulo}>
-          Porcentagem
-        </p>
-        <input type="number" className={styles.input}/>
+      <div className={styles.container}>
+        <div className={styles.item1}>
+          <h1 className={styles.title}>Calculadora</h1>
+          <p className={styles.item1Text}>Modelo</p>
+          <input type="text" className={`${styles.input}`} />
+          <p className={styles.item1Text}>Qtd. Veículos</p>
+          <input type="number" className={`${styles.input}`} />
+          <p className={styles.item1Text}>Distância</p>
+          <input type="number" className={`${styles.input}`} />
+          <p className={styles.item1Text}>Porcentagem</p>
+          <input type="number" className={`${styles.input}`} />
+        </div>
+
+        <div className={styles.item2}>
+          <h1 className={styles.title}>Resultados</h1>
+          <p className={styles.item1Text}>L50</p>
+          <input type="number" className={`${styles.input}`} disabled/>
+          <p className={styles.item2Text}>L90</p>
+          <input type="number" className={`${styles.input}`} disabled/>
+          <p className={styles.item2Text}>Leq</p>
+          <input type="number" className={`${styles.input}`} disabled />
         </div>
       </div>
-
-      <div className={styles.item2}>
-        <h3 className={styles.titulo}>
-          Resultado
-        </h3>
-        <p className={styles.subtitulo}>
-          L50
-        </p>
-        <input type="number" className={styles.input}/>
-        <p className={styles.subtitulo}>
-          L90
-        </p>
-        <input type="number" className={styles.input}/>
-        <p className={styles.subtitulo}>
-           Leq
-        </p>
-        <input type="number" className={styles.input}/>
-      </div>
     </div>
-  )
+  );
 }
 
-export default Calculator
